c5e733d35730ff1db89a05209902997fc293fe62
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2023-05-05 15:55:33 +0800

    Update Code

--- src/Debug/File_Debug.java
+++ src/Debug/File_Debug.java
@@ -6,8 +6,8 @@ import java.io.File;
 
 public class File_Debug {
     public static void main(String[] args){
-        String input_file = "Compile_Test/get_LL1_table_test.txt";
-        String output_file = "Compile_Test/output.txt";
+        String input_file = "Compile_Test/LL1_table_test/get_LL1_table_test.txt";
+        String output_file = "Compile_Test/LL1_table_test/output.txt";
         LL1_list V = FileIO.read_from_file(new File(input_file));
         FileIO.write_to_file(new File(output_file), V);
     }
