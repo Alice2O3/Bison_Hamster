d2b30776808aa60b9559e3fd139febc46691b277
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2023-05-14 18:54:59 +0800

    Update Code

--- src/C_Bison/Language/Easy_C/Lexing/Workflow.java
+++ src/C_Bison/Language/Easy_C/Lexing/Workflow.java
@@ -11,11 +11,40 @@ public class Workflow {
     private void process_tokens(List<DFA_lexing> lexing_list){
         StringBuilder sb = new StringBuilder();
         /*
-            ["0", Integer]
             ["int", Int]
+            ["main", Identifier]
+            ["(", Left_small_bracket]
+            [")", Right_small_bracket]
+            ["{", Left_big_bracket]
+            ["int", Int]
+            ["a", Identifier]
+            [",", Comma]
+            ["b", Identifier]
+            [",", Comma]
+            ["c", Identifier]
+            [";", Statement_end]
+            ["a", Identifier]
+            ["=", Assign]
+            ["123", Integer]
+            [";", Statement_end]
+            ["b", Identifier]
+            ["=", Assign]
+            ["456", Integer]
+            [";", Statement_end]
+            ["c", Identifier]
+            ["=", Assign]
+            ["a", Identifier]
+            ["+", Plus]
+            ["b", Identifier]
+            [";", Statement_end]
+            ["return", Return]
+            ["0", Integer]
+            [";", Statement_end]
+            ["}", Right_big_bracket]
+            ["EOF", EOF]
         */
         for(DFA_lexing lexing : lexing_list) {
-            sb.append("[").append("\"").append(lexing.token).append("\"").append(", ").append(Tokens.Map[lexing.token_type]).append("]\n");
+            sb.append("[").append("$").append(lexing.token).append("$").append(", ").append(Tokens.Map[lexing.token_type]).append("]\n");
         }
         token_info = sb.toString();
     }
