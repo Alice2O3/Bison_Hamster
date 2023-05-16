976861c8c8e2645a7d70d9c427a26e148b72af85
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2023-05-16 16:47:54 +0800

    Refactor Code

--- src/C_Bison/Language/Easy_C/Lexing/Render.java
+++ src/C_Bison/Language/Easy_C/Lexing/Render.java
@@ -4,43 +4,12 @@ import C_Bison.Language.Easy_C.Tokens;
 import C_Flex.Types.*;
 
 public class Render {
-    public static String Convert_Tokens_to_str(DFA_lexing_list Token_pair_list){
+    public static String Convert_Tokens_to_str(DFA_lexing_list tokens_list){
         StringBuilder sb = new StringBuilder();
-        /*
-            ["int", Int]
-            ["main", Identifier]
-            ["(", Left_small_bracket]
-            [")", Right_small_bracket]
-            ["{", Left_big_bracket]
-            ["int", Int]
-            ["a", Identifier]
-            [",", Comma]
-            ["b", Identifier]
-            [",", Comma]
-            ["c", Identifier]
-            [";", Statement_end]
-            ["a", Identifier]
-            ["=", Assign]
-            ["123", Integer]
-            [";", Statement_end]
-            ["b", Identifier]
-            ["=", Assign]
-            ["456", Integer]
-            [";", Statement_end]
-            ["c", Identifier]
-            ["=", Assign]
-            ["a", Identifier]
-            ["+", Plus]
-            ["b", Identifier]
-            [";", Statement_end]
-            ["return", Return]
-            ["0", Integer]
-            [";", Statement_end]
-            ["}", Right_big_bracket]
-            ["EOF", EOF]
-        */
-        for(DFA_lexing lexing : Token_pair_list.val) {
-            sb.append("[").append("\"").append(lexing.token).append("\"").append(", ").append(Tokens.Map[lexing.token_type]).append("]\n");
+        int token_index = 0;
+        for(DFA_lexing l : tokens_list.val){
+            sb.append(String.format("[@%d,%d:%d='%s',<%s>,%d:%d]\n", token_index, l.l_index_global, l.r_index_global, l.token, Tokens.Map[l.token_type], l.line_index, l.row_index));
+            token_index++;
         }
         return sb.toString();
     }
